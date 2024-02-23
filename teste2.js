b5d29f9fcd935f7c4a4f14fa53b13f689b0daf22
manu <manuuheela2017@gmail.com>
2024-02-23 11:47:10 -0300

    dia 23.02

---
+++ teste2.js
@@ -0,0 +1,8 @@
+let filme = {
+    nome: "Anatomy of a fall",
+    nota: 8.5,
+    genero: "Drama policial"
+}
+console.log(filme.nome);
+console.log(filme.nota);
+console.log(filme.genero)
---
+++ teste3.js
@@ -0,0 +1,8 @@
+let nomes = ["Manuela", "Arthur", "Lissa"]
+let copia_indice_zero = nomes.slice()[0][0]
+console.log(copia_indice_zero)
+let copia_indice_um = nomes.slice()[1][0]
+console.log(copia_indice_um)
+let copia_indice_dois = nomes.slice()[2][0]
+console.log(copia_indice_dois)
+
